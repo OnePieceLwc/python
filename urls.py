8f76fa39d9a2bb73463fe0805cd0983972fd2bab
LucianaiB <120895319+OnePieceLwc@users.noreply.github.com>
2025-03-22 01:27:52 +0800

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,5 @@
+URL_LIST = [
+    "https://blog.csdn.net/lwcwam/article/details/146407707",
+    "https://blog.csdn.net/lwcwam/article/details/146427747",
+    "https://blog.csdn.net/lwcwam/article/details/146430280"
+]
