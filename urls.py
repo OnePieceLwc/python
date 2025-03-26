92d4937992328f4ecbb3e9beee30e10ec8d9caa8
LucianaiB <120895319+OnePieceLwc@users.noreply.github.com>
2025-03-26 11:32:47 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://blog.csdn.net/2301_76341691/article/details/146398224",
-    "https://blog.csdn.net/2301_76341691/article/details/146398224",
+    "https://lucianaib.blog.csdn.net/article/details/146445900",
+    "https://lucianaib.blog.csdn.net/article/details/146430280",
     "https://lucianaib.blog.csdn.net/article/details/146427747"
 ]
