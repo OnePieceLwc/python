6a339a822166b3701440400da9749641b430364d
LucianaiB <120895319+OnePieceLwc@users.noreply.github.com>
2025-05-14 12:12:36 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
-    "https://lucianaib.blog.csdn.net/article/details/145167323",
+    "https://blog.csdn.net/lwcwam/article/details/147933545",
     "https://lucianaib.blog.csdn.net/article/details/147805601",
     "https://lucianaib.blog.csdn.net/article/details/147289819"
 ]
