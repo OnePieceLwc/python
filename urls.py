babeaa387f038e80fc3ec0c6744aaf9d8408668a
LucianaiB <120895319+OnePieceLwc@users.noreply.github.com>
2025-05-31 20:35:16 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 URL_LIST = [
     "https://blog.csdn.net/lwcwam/article/details/147933545",
-    "https://lucianaib.blog.csdn.net/article/details/147805601",
-    "https://lucianaib.blog.csdn.net/article/details/146445900"
+    "https://blog.csdn.net/lwcwam/article/details/147933545",
+    "https://lucianaib.blog.csdn.net/article/details/148077253"
 ]
